745629a1f7ae06f96ca115cfb4ec7500f2f6b93d
hoshino <2683442914@qq.com>
2019-03-11 12:17:58 +0800

    O(nlogn)快排sort(),reverse().

--- 215.数组中的第k个最大元素.cpp
+++ 215.数组中的第k个最大元素.cpp
@@ -29,7 +29,7 @@
  * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
  * 
  */
-//sort(),reverse().O(nlogn)
+//sort(),reverse().O(nlogn)  
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
