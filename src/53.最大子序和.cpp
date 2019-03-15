4d9881d226e8ae144b8afccc7af6ff60057fbf98
hoshinotsuki <2683442914@qq.com>
2019-03-15 11:37:45 +0800

    分治法。max(divide,merge)。2*O(n/2)+O(1)=O(n)。

--- src/53.最大子序和.cpp
+++ src/53.最大子序和.cpp
@@ -25,19 +25,43 @@
  * 如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
  * 
  */
-//  
+//  分治法。2*O(n/2)+O(1)=O(n)
+//divide：用递归来分别求左右子集的max
+//merge:用dp来从左右分别线性探索，加上mid
+//最后找到divide和merge中最大的就是要求的值。
 class Solution {
 public:
-  int maxSubArray(vector<int>& nums) {
-        int sum = nums[0] ;
-        int maxSum= sum;
-        
-        for(int num = 1; num < nums.size(); num++){
-            sum = max(nums[num], (sum + nums[num]));
-            maxSum = max(sum, maxSum);
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArray(nums, 0, nums.size() - 1);
+    }
+private:
+    int maxSubArray(vector<int>& nums, int l, int r) {
+        if (l > r) {
+            return INT_MIN;
         }
+
+        //divide:
+        int m = l + (r - l) / 2;
+        int lmax = maxSubArray(nums, l, m - 1);//left
+        int rmax = maxSubArray(nums, m + 1, r);//right
+        int partmax = max(lmax, rmax);//not including mid
         
-        return maxSum;
+        //Merge:
+        int ml= 0,mr = 0;
+        //left-DP:O(n/2)
+        for (int i = m - 1, sum = 0; i >= l; i--) {
+            sum += nums[i];
+            ml = max(sum, ml);
+        }
+        //right-DP:O(n/2)
+        for (int i = m + 1, sum = 0; i <= r; i++) {
+            sum += nums[i];
+            mr = max(sum, mr);
+        }
+        //Merge-including mid,for continuous subset:O(1)
+        int contmax=ml + mr + nums[m];
+
+        return max(partmax,contmax);
+        //2*O(n/2)+O(1)=O(n)
     }
 };
-
