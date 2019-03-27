084aa995769201ceebfac2e6b86770a1f5c8266c
hoshinotsuki <2683442914@qq.com>
2019-03-27 08:56:45 +0800

    left=0,right=total;right-sum[i],if i;left+nums[i]

---
+++ src/724.寻找数组的中心索引.py
@@ -0,0 +1,12 @@
+//left=0,right=total;right-sum[i],if(==),return i;left+nums[i];return -1
+lass Solution(object):
+    def pivotIndex(self, nums):
+        # Time: O(n)
+        # Space: O(1)
+        left, right = 0, sum(nums)
+        for index, num in enumerate(nums):
+            right -= num
+            if left == right:
+                return index
+            left += num
+        return -1
