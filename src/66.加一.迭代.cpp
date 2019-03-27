71c6d58088d8dfca479dcc34b2d8d58126047312
hoshinotsuki <2683442914@qq.com>
2019-03-27 10:18:29 +0800

    迭代 O(1)space,O(n)time

---
+++ src/66.加一.迭代.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=cpp
+ *
+ * [66] 加一
+ *
+ * https://leetcode-cn.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (37.48%)
+ * Total Accepted:    42.9K
+ * Total Submissions: 113K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ * 
+ * 
+ */
+//迭代 O(1)space,O(n)time
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {   //未考虑前缀0的情况
+        for(int i = digits.size() - 1; i >= 0; i--)   
+        {
+            if(digits[i] != 9)
+            {
+                digits[i] ++;
+                break;
+            }
+            digits[i] = 0;
+        }
+        if(digits[0] == 0)
+            digits.insert(digits.begin(), 1);
+        return digits;
+        
+    }
+};
+
