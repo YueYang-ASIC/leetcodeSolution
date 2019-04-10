498ce944bf755405854a4d189969ef71c9d8340f
hoshinotsuki <2683442914@qq.com>
2019-04-10 13:45:03 +0800

    bit manipulation

--- src/136.只出现一次的数字bit.cpp
+++ src/136.只出现一次的数字bit.cpp
@@ -34,10 +34,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
 		int ret=0;
-        for(auto num:nums)
-        {
-            ret=ret^num;
-        }
+        for(auto num:nums) ret=ret^num; 
 		return ret;
 	}
 };
