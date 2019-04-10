89d7ee9f5f5e37bee5c5b812038bbca323d220ad
hoshinotsuki <2683442914@qq.com>
2019-04-10 19:54:41 +0800

    hash table /set

---
+++ 349.两个数组的交集.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=cpp
+ *
+ * [349] 两个数组的交集
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (59.61%)
+ * Total Accepted:    17.3K
+ * Total Submissions: 28.2K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]
+ * 
+ * 说明:
+ * 
+ * 
+ * 输出结果中的每个元素一定是唯一的。
+ * 我们可以不考虑输出结果的顺序。
+ * 
+ * 
+ */
+//hash table /set
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> s(nums1.begin(),nums1.end()),res;
+        for(auto num : nums2) 
+            if(s.count(num)) res.insert(num); 
+        return vector<int>(res.begin(),res.end());
+    }
+};
+
